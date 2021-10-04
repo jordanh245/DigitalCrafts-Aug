aaa69d924a2b4605e0c04f1c64d6d80f1236dffb
Jordan Harris <Jordanharris245@gmail.com>
2021-10-04 17:48:01 -0400

    Update SidebarStyle.js

--- Week9/Day4/form-validations-2/src/styled-components/SidebarStyle.js
+++ Week9/Day4/form-validations-2/src/styled-components/SidebarStyle.js
@@ -5,20 +5,14 @@ import {Link} from "react-router-dom"
 export const SidebarContainer = styled.div`
 display: flex;
 flex-direction: column;
-
 grid-area: sidebar;
 width: 100%;
-
-
-
 
 `
 
 export const SidebarNav = styled(Link)`
-
 background-color: #124D40;
 height: 100vh;
-
 text-align: center;
 color: white;
 
